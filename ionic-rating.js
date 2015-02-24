f8a134be9fb947be1d811f7f289d12d95f80c106
Pulkit Kathuria <pulkit.kathuria@g-pc-5490.intra.gree-office.net>
2015-02-24 18:09:47 +0900

    ion-ios7-star-outline is long dead ? bumped icon to v2.0.0 ionicon * http://ionicons.com/cheatsheet.html * Note: min.js still corresponds to old icon

--- ionic-rating.js
+++ ionic-rating.js
@@ -74,7 +74,7 @@
         onLeave: '&'
       },
       controller: 'RatingController',
-      template: '<ul class="rating" ng-mouseleave="reset()" ng-keydown="onKeydown($event)">' + '<li ng-repeat="r in range track by $index" ng-click="rate($index + 1)"><i class="icon" ng-class="$index < value && (r.stateOn || \'ion-ios7-star\') || (r.stateOff || \'ion-ios7-star-outline\')"></i></li>' + '</ul>',
+      template: '<ul class="rating" ng-mouseleave="reset()" ng-keydown="onKeydown($event)">' + '<li ng-repeat="r in range track by $index" ng-click="rate($index + 1)"><i class="icon" ng-class="$index < value && (r.stateOn || \'ion-ios-star\') || (r.stateOff || \'ion-ios-star-outline\')"></i></li>' + '</ul>',
       replace: true,
       link: function(scope, element, attrs, ctrls) {
         var ngModelCtrl, ratingCtrl;
